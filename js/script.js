7106ac318ce34713d663151741fd2b7de830df5f
asdcom <asda@a.com>
2022-05-07 22:33:47 +0500

    create media scr.full

--- js/script.js
+++ js/script.js
@@ -39,6 +39,69 @@ setTimeout(typeText, 4000)
 
 // ********************************
 
-// var typed = new Typed(".typing", {
-//     strings:
-// })
+const nav = document.querySelector(".nav"),
+      navList = nav.querySelectorAll("li"),
+      totalNavList = navList.length;
+      allSection = document.querySelectorAll(".section"),
+      totalSection = allSection.length
+      for(let i = 0; i<totalNavList; i++)
+      {
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click", function()
+        {
+
+            
+            for(let j=0; j<totalNavList; j++){
+                if(navList[j].querySelector("a").classList.contains("acctive"))
+                      {
+                          allSection[j].classList.add("back__section");
+                    }
+                    navList[j].querySelector("a").classList.remove("acctive");
+                    }
+                    this.classList.add("acctive")
+                  showSection(this);
+                  if(window.innerWidth < 1200){
+                asideSectionTogglerBtn();
+                }
+        })
+      }
+      function showSection(element)
+      {
+          for(let i=0; i<totalSection; i++) {
+              allSection[i].classList.remove("acctive")
+          }
+          const target=element.getAttribute("href").split("#")[1];
+          document.querySelector("#" + target).classList.add("acctive")
+      }
+    function updataNav(element)
+    {
+        for(let i=0; i<totalNavList; i++)
+        {
+            navList[i].querySelector("a").classList.remove("acctive");
+            const target = element.getAttribute("href").split("#")[1];
+            if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1])
+            {
+            navList[i].querySelector("a").classList.add("acctive");
+
+            }
+        }
+    }
+    document.querySelector(".hire__me").addEventListener("click", function() 
+    {
+        const sectionIndex = this.getAttribute("data-section-index");
+        showSection(this);
+        updataNav(this);
+    })
+    const navTogglerBtn = document.querySelector(".nav__toggler"),
+      aside = document.querySelector(".aside");
+      navTogglerBtn.addEventListener("click", () => {
+          asideSectionTogglerBtn();
+      }) 
+      function asideSectionTogglerBtn()
+      {
+          aside.classList.toggle("open");
+          navTogglerBtn.classList.toggle("open")
+          for(let i=0; i<totalSection; i++) {
+              allSection[i].classList.toggle("open");
+          }   
+      }
